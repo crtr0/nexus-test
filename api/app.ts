8b742743108ed05aad0b36161ab5679233d4aa03
Carter Rabasa <carter.rabasa@gmail.com>
2020-04-13 13:53:37 -0700

    updates for deployment to Heroku

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,11 @@
-import { use } from 'nexus'
+import { settings, use } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
 
 // Enables the Prisma plugin
 use(prisma())
+
+settings.change({
+    server: {
+        playground: true
+    }
+})
